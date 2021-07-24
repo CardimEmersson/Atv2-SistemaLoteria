e33b2471d12f1a924503bde32a760ad3265ba1b3
Emersson Cardim Mota <63078277+CardimEmersson@users.noreply.github.com>
2021-07-24 11:46:55 -0300

    agora é possível adicionar varios jogos

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-# Armazena o nome do jogador
-playerName = input("Digite o nome do jogador: ")
+game = []
 
-luckyNumbers = []
 while True:
     try:
-      for count in range(2):
-        luckyNumber = int(input(f"Informe o {count + 1}º numero da sorte entre 0 e 9: "))
-        if not 0 <= luckyNumber <= 9:
-            raise ValueError("Numero da sorte inválido")
-        luckyNumbers.append(luckyNumber)
+
+        playerName = input("Digite o nome do jogador: ")
+        luckyNumbers = []
+
+        for count in range(2):
+            luckyNumber = int(
+                input(f"Informe o {count + 1}º numero da sorte entre 0 e 9: "))
+            if not 0 <= luckyNumber <= 9:
+                raise ValueError("Numero da sorte inválido")
+            luckyNumbers.append(luckyNumber)
+
+        game.append([playerName, luckyNumbers])
+
+        newGame = input("Adicionar novo jogo? sim ou nao: ")
+        if newGame != "sim":
+            break
     except ValueError as e:
         print("Valor inválido: ", e)
-    else:
-        break
 
-print(playerName)
-print(luckyNumbers)
+print(game)
