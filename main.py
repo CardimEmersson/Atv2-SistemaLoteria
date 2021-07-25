bd48f7b396d274278af0c3384dd726dd7dd96a4b
Emersson Cardim Mota <63078277+CardimEmersson@users.noreply.github.com>
2021-07-25 09:49:13 -0300

    verifica se alguem foi sorteado e imprime seu nome

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import random
 
-game = []
+games = []
 
 while True:
     try:
-
         playerName = input("Digite o nome do jogador: ")
+        if not playerName.strip():
+            raise ValueError("Nome inválido")
         luckyNumbers = []
 
         for count in range(2):
             luckyNumber = int(
-                input(f"Informe o {count + 1}º numero da sorte entre 0 e 9: "))
+                input(
+                    f"Informe seu {count + 1}º numero da sorte entre 0 e 9: "))
             if not 0 <= luckyNumber <= 9:
                 raise ValueError("Numero da sorte inválido")
             luckyNumbers.append(luckyNumber)
 
-        game.append([playerName, luckyNumbers])
+        games.append([playerName, luckyNumbers])
 
         newGame = input("Adicionar novo jogo? sim ou nao: ")
         if newGame != "sim":
@@ -23,10 +25,20 @@ while True:
     except ValueError as e:
         print("Valor inválido: ", e)
 
-print(game)
-
 drawnNumbers = []
-drawnNumbers.append(int(random.random() * 9))
-drawnNumbers.append(int(random.random() * 9))
+drawnNumbers.append(int(random.random() * 2))
+drawnNumbers.append(int(random.random() * 2))
 
-print(drawnNumbers)
+print("numeros sorteados: ", drawnNumbers)
+
+drawnPlayers = []
+for game in games:
+    intersection = list(set(drawnNumbers) & set(game[1]))
+    if len(intersection) == 2:
+        drawnPlayers.append(game[0])
+
+if (drawnPlayers):
+    for drawnPlayer in drawnPlayers:
+        print("Jogador sorteado: ", drawnPlayer)
+else:
+    print("Nenhum jogador sorteado!")
